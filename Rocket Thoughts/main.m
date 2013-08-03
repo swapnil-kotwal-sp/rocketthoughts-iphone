662fe7c0781dabf3a2b1a501c7a74ff6f2ea6ec7
Pallavi Kamat <pallavi@weboniselab.com>
2013-08-03 13:59:27 +0530

    Initial Commit

---
+++ Rocket Thoughts/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Rocket Thoughts
+//
+//  Created by Mac-4 on 03/08/13.
+//  Copyright (c) 2013 Pallavi Kamat. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
