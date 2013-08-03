c69bac77dc5163ffc8a701552fc2650e1ef3aa07
Pallavi Kamat <pallavi@weboniselab.com>
2013-08-03 14:37:36 +0530

    added thought lists

---
+++ Rocket Thoughts/ThoughtsListViewController.h
@@ -0,0 +1,13 @@
+//
+//  ThoughtsListViewController.h
+//  Rocket Thoughts
+//
+//  Created by Mac-4 on 03/08/13.
+//  Copyright (c) 2013 Pallavi Kamat. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ThoughtsListViewController : UIViewController
+
+@end
---
+++ Rocket Thoughts/ThoughtsListViewController.m
@@ -0,0 +1,38 @@
+//
+//  ThoughtsListViewController.m
+//  Rocket Thoughts
+//
+//  Created by Mac-4 on 03/08/13.
+//  Copyright (c) 2013 Pallavi Kamat. All rights reserved.
+//
+
+#import "ThoughtsListViewController.h"
+
+@interface ThoughtsListViewController ()
+
+@end
+
+@implementation ThoughtsListViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
